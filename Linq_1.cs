e1f1370337b4a7edafdfca0c19d53f27c749433e
Marcelo Henrique <marcelo_assis98@hotmail.com>
2020-01-14 08:50:18 -0300

    Linq C#

---
+++ Linq_1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Linq_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Define the query exression
+            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            //Execute the query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}

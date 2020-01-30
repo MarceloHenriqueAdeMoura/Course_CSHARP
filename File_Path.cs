1fb13d26f8750ef8f92904a10fb20b1c34d1c3be
Marcelo Henrique <marcelo_assis98@hotmail.com>
2020-01-30 08:23:05 -0300

    File e Path C#

---
+++ File_Path.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+
+namespace File_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Marcelo\Documents\VisualStudioCode Projects\c#\arquivos\arquivo1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+        }
+    }    
+}
